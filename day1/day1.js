44919bc396794cb2635b483b53b9aae77950e955
Katie Fugate <katiiierochelle@yahoo.com>
2023-12-07 14:07:54 -0800

    day1part2

--- day1/day1.js
+++ day1/day1.js
@@ -20,14 +20,17 @@ function part2() {
     let data = array.filter(item => item !== '');
     let spelledNumbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let total = 0;
+
     data.forEach(line => {
         let firstIndex = line.split('').findIndex((item) => parseInt(item));
         let lastIndex = line.split('').findLastIndex((item) => parseInt(item));
         let first = line[firstIndex];
         let last = line[lastIndex];
+
         spelledNumbers.forEach((number, i) => {
             let firstPosition = line.search(number);
             let lastPosition = line.lastIndexOf(number);
+
             if (firstPosition < firstIndex && firstPosition >= 0 || firstIndex === -1) {
                 firstIndex = firstPosition;
                 first = i + 1
@@ -35,6 +38,7 @@ function part2() {
                 lastIndex = firstPosition;
                 last = i + 1
             }
+
             if(lastPosition > lastIndex) {
                 lastIndex = lastPosition;
                 last = i + 1
